5cb731b4a678e83e760cc367d426b59f592ef002
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-12-04 03:31:32 +0100

    a

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Every Compat
+# Stone Zone
 
 Tired of incomplete wood sets? Well you came to the right place!
 
@@ -9,36 +9,3 @@ Every block type added is available in ALL wood types.
 With this mod you won't need extra compatibility addons and instead only rely on a single universal one! It's all automatic!
 
 ## Supported Mods:
-
-- Quark
-- Another Furniture
-- Farmers Delight
-- Decorative Blocks
-- Architects Palette
-- Valhelsia Structures
-- Storage Drawers
-- Twilight Forest
-- Twigs
-- Macaw Mods
-- Woodworks
-- Backpacked
-- Builders Delight
-- Ex Nihilo
-- Lauchs New Shutters
-- Pokecube
-- Premium Wood
-- Productive Bees
-- Woodster
-- Workshop
-- Xerca Mod
-- Camp Chair
-- Chipped
-- Exline Furnitures
-- Friends And Foes
-- Hearth And Home
-- Missing Wilds
-- Infinity Buttons
-- Create
-- More Chest Variatns
-- Variant Vanilla Block
-- MrCrayFish's Refurbished Furniture
--- common/src/main/java/net/mehvahdjukaar/stone_zone/StoneZone.java
+++ common/src/main/java/net/mehvahdjukaar/stone_zone/StoneZone.java
@@ -1,8 +1,16 @@
 package net.mehvahdjukaar.stone_zone;
 
+import net.mehvahdjukaar.every_compat.api.CompatModule;
+import net.mehvahdjukaar.every_compat.api.EveryCompatAPI;
+import net.mehvahdjukaar.moonlight.api.platform.PlatHelper;
+import net.mehvahdjukaar.moonlight.api.set.BlockSetAPI;
+import net.mehvahdjukaar.stone_zone.type.StoneTypeRegistry;
 import net.minecraft.resources.ResourceLocation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class StoneZone {
     public static final String MOD_ID = "stonezone";
@@ -14,8 +22,18 @@ public class StoneZone {
     public static final Logger LOGGER = LogManager.getLogger("Stone Zone");
 
 
-    public static void init(){
+    public static void init() {
+        BlockSetAPI.registerBlockSetDefinition(StoneTypeRegistry.INSTANCE);
 
+
+        //addIfLoaded("every_compat", EveryCompatModule::new);
+    }
+
+    private void addIfLoaded(String modId, Supplier<Function<String, CompatModule>> moduleFactory) {
+        if (PlatHelper.isModLoaded(modId)) {
+            CompatModule module = moduleFactory.get().apply(modId);
+            EveryCompatAPI.registerModule(module);
+        }
     }
 
 }
