d9c55322754eea7e7592af5f24576b1c7478e072
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2024-12-05 01:43:58 -0700

    SZRegistry: Changed Supplier<> to RegSupplier<>

--- common/src/main/java/net/mehvahdjukaar/stone_zone/SZRegistry.java
+++ common/src/main/java/net/mehvahdjukaar/stone_zone/SZRegistry.java
@@ -1,5 +1,6 @@
 package net.mehvahdjukaar.stone_zone;
 
+import net.mehvahdjukaar.moonlight.api.misc.RegSupplier;
 import net.mehvahdjukaar.moonlight.api.platform.RegHelper;
 import net.mehvahdjukaar.stone_zone.configs.SZConfigs;
 import net.mehvahdjukaar.stone_zone.misc.AllStonesItem;
@@ -18,14 +19,12 @@ public class SZRegistry {
     public static final Supplier<AllStonesItem> ALL_STONES = RegHelper.registerItem(StoneZone.res("all_stones"), AllStonesItem::new);
 
     @Nullable
-    public static final Supplier<CreativeModeTab> MOD_TAB = SZConfigs.TAB_ENABLED.get() ?
+    public static final RegSupplier<CreativeModeTab> MOD_TAB = SZConfigs.TAB_ENABLED.get() ?
             RegHelper.registerCreativeModeTab(StoneZone.res("stonezone"),
                     true,
-                    (builder) -> {
-                        builder.icon(() -> ALL_STONES.get().getDefaultInstance())
-                                .backgroundSuffix("item_search.png")
-                                .title(Component.translatable("itemGroup.stonezone.stonezone"))
-                                .build();
-                    })
+                    builder -> builder.icon(() -> ALL_STONES.get().getDefaultInstance())
+                            .backgroundSuffix("item_search.png")
+                            .title(Component.translatable("itemGroup.stonezone.stonezone"))
+                            .build())
             : null;
 }
