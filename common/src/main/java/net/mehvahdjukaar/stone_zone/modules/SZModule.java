baf9b582f42ae30b4e01b0dc81877a756afa107a
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2024-12-05 01:46:11 -0700

    SZModule: Updated getUniqueTab()

--- common/src/main/java/net/mehvahdjukaar/stone_zone/modules/SZModule.java
+++ common/src/main/java/net/mehvahdjukaar/stone_zone/modules/SZModule.java
@@ -1,17 +1,18 @@
 package net.mehvahdjukaar.stone_zone.modules;
 
 import net.mehvahdjukaar.every_compat.api.SimpleModule;
-import net.mehvahdjukaar.stone_zone.StoneZone;
+import net.mehvahdjukaar.stone_zone.SZRegistry;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.CreativeModeTab;
+
 
 public class SZModule extends SimpleModule {
     public SZModule(String modId, String shortId) {
         super(modId, shortId);
     }
 
-    @Override
-    public ResourceKey<CreativeModeTab> getUniqueTab(){
-        return StoneZone.TAB.key();
+    @SuppressWarnings("DataFlowIssue")
+    public ResourceKey<CreativeModeTab> getUniqueTab() {
+        return SZRegistry.MOD_TAB.getKey();
     }
 }
