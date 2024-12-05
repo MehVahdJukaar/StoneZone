d49eec316f2f8a1c8eb92b385f039131482c0cee
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2024-12-04 22:39:45 -0700

    NEW CLASS: SZRegistry

---
+++ common/src/main/java/net/mehvahdjukaar/stone_zone/SZRegistry.java
@@ -0,0 +1,31 @@
+package net.mehvahdjukaar.stone_zone;
+
+import net.mehvahdjukaar.moonlight.api.platform.RegHelper;
+import net.mehvahdjukaar.stone_zone.configs.SZConfigs;
+import net.mehvahdjukaar.stone_zone.misc.AllStonesItem;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Supplier;
+
+public class SZRegistry {
+
+    public static void init() {
+
+    }
+
+    public static final Supplier<AllStonesItem> ALL_STONES = RegHelper.registerItem(StoneZone.res("all_stones"), AllStonesItem::new);
+
+    @Nullable
+    public static final Supplier<CreativeModeTab> MOD_TAB = SZConfigs.TAB_ENABLED.get() ?
+            RegHelper.registerCreativeModeTab(StoneZone.res("stonezone"),
+                    true,
+                    (builder) -> {
+                        builder.icon(() -> ALL_STONES.get().getDefaultInstance())
+                                .backgroundSuffix("item_search.png")
+                                .title(Component.translatable("itemGroup.stonezone.stonezone"))
+                                .build();
+                    })
+            : null;
+}
