2c1678f8661146aa4aeb4b89762e0a29a774b21b
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2024-12-05 02:49:56 -0700

    StoneZoneFabric: Updated but not finished

--- fabric/src/main/java/net/mehvahdjukaar/stone_zone/fabric/StoneZoneFabric.java
+++ fabric/src/main/java/net/mehvahdjukaar/stone_zone/fabric/StoneZoneFabric.java
@@ -2,11 +2,26 @@ package net.mehvahdjukaar.stone_zone.fabric;
 
 import net.fabricmc.api.ModInitializer;
 import net.mehvahdjukaar.stone_zone.StoneZone;
+import net.mehvahdjukaar.stone_zone.StoneZoneCommon;
 
-public class StoneZoneFabric implements ModInitializer {
+public class StoneZoneFabric extends StoneZoneCommon implements ModInitializer {
 
     @Override
     public void onInitialize() {
         StoneZone.init();
+    }
+
+    @Override
+    protected void addModules() {
+
+
+//!! =============================================== Add Other Compat Mods ========================================== \\
+
+
+//!! =================================================== Add Modules ================================================ \\
+
+
+//!! ====================================================== OTHERS ================================================== \\
+
     }
 }
