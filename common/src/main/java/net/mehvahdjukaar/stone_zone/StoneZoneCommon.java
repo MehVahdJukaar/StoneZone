6cb3830d81a9f20f59ea6a7c812af243a6a071d5
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2024-12-06 14:46:16 -0700

    StoneZoneCommon: Added TwigsModule & CreateModule

--- common/src/main/java/net/mehvahdjukaar/stone_zone/StoneZoneCommon.java
+++ common/src/main/java/net/mehvahdjukaar/stone_zone/StoneZoneCommon.java
@@ -1,10 +1,16 @@
 package net.mehvahdjukaar.stone_zone;
 
+import net.mehvahdjukaar.stone_zone.modules.create.CreateModule;
+import net.mehvahdjukaar.stone_zone.modules.twigs.TwigsModule;
+
+import static net.mehvahdjukaar.stone_zone.StoneZone.addIfLoaded;
+
 public class StoneZoneCommon {
 
     protected void initialize() {
         StoneZone.init();
 
+        this.addModules();
     }
 
     protected void addModules() {
@@ -14,7 +20,8 @@ public class StoneZoneCommon {
 
 
 //!! =================================================== Add Modules ================================================ \\
-
+        addIfLoaded("twigs", () -> TwigsModule::new);
+        addIfLoaded("create", () -> CreateModule::new);
 
 //!! ====================================================== OTHERS ================================================== \\
 
