b65dac0e356b77ad9d4187d6e0fa1e333002394f
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2024-12-07 16:16:35 -0700

    StoneZone: Updated & Cleanup

--- common/src/main/java/net/mehvahdjukaar/stone_zone/StoneZone.java
+++ common/src/main/java/net/mehvahdjukaar/stone_zone/StoneZone.java
@@ -1,28 +1,14 @@
 package net.mehvahdjukaar.stone_zone;
 
-import net.mehvahdjukaar.every_compat.EveryCompatCommon;
-import net.mehvahdjukaar.every_compat.api.CompatModule;
-import net.mehvahdjukaar.every_compat.api.EveryCompatAPI;
-import net.mehvahdjukaar.moonlight.api.platform.PlatHelper;
-import net.mehvahdjukaar.moonlight.api.platform.RegHelper;
+import net.mehvahdjukaar.every_compat.EveryCompat;
 import net.mehvahdjukaar.moonlight.api.set.BlockSetAPI;
 import net.mehvahdjukaar.stone_zone.configs.SZConfigs;
-import net.mehvahdjukaar.stone_zone.misc.AllStonesItem;
-import net.mehvahdjukaar.stone_zone.modules.twigs.TwigsModule;
 import net.mehvahdjukaar.stone_zone.type.StoneTypeRegistry;
-import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.CreativeModeTab;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.function.Function;
-import java.util.function.Supplier;
-
-public class StoneZone {
+public class StoneZone extends EveryCompat {
     public static final String MOD_ID = "stonezone";
     public static final Logger LOGGER = LogManager.getLogger("Stone Zone");
 
@@ -37,11 +23,5 @@ public class StoneZone {
         return new ResourceLocation(MOD_ID, name);
     }
 
-    public static void addIfLoaded(String modId, Supplier<Function<String, CompatModule>> moduleFactory) {
-        if (PlatHelper.isModLoaded(modId)) {
-            CompatModule module = moduleFactory.get().apply(modId);
-            EveryCompatAPI.registerModule(module);
-        }
-    }
 
 }
