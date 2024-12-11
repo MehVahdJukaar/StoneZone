0b4aa40aef0bc8b618e36cc76c110f933b722044
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2024-12-11 16:56:10 -0700

    StoneType & StoneTypeRegistry: Updated to support TFC

--- common/src/main/java/net/mehvahdjukaar/stone_zone/type/StoneTypeRegistry.java
+++ common/src/main/java/net/mehvahdjukaar/stone_zone/type/StoneTypeRegistry.java
@@ -2,20 +2,29 @@ package net.mehvahdjukaar.stone_zone.type;
 
 import net.mehvahdjukaar.moonlight.api.events.AfterLanguageLoadEvent;
 import net.mehvahdjukaar.moonlight.api.set.BlockTypeRegistry;
+import net.mehvahdjukaar.moonlight.core.Moonlight;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
+@SuppressWarnings("unused")
 public class StoneTypeRegistry extends BlockTypeRegistry<StoneType> {
 
     public static final StoneTypeRegistry INSTANCE = new StoneTypeRegistry();
 
     public static final StoneType STONE_TYPE = new StoneType(new ResourceLocation("stone"), Blocks.STONE);
     public static final StoneType ANDESITE_TYPE = new StoneType(new ResourceLocation("andesite"), Blocks.ANDESITE);
+
+//    public static Collection<StoneType> getType() {
+//        return INSTANCE.getValues();
+//    }
 
     public StoneTypeRegistry() {
         super(StoneType.class, "stone_type");
@@ -33,13 +42,28 @@ public class StoneTypeRegistry extends BlockTypeRegistry<StoneType> {
     }
 
     @Override
-    public Optional<StoneType> detectTypeFromBlock(Block block, ResourceLocation blockId) {
-        String path = blockId.getPath();
-        if (path.endsWith("_bricks") && block.defaultBlockState().instrument() == NoteBlockInstrument.BASEDRUM) {
+    public Optional<StoneType> detectTypeFromBlock(Block baseblock, ResourceLocation baseRes) {
+        String path = baseRes.getPath();
+        // Support TerraFirmaCraft (TFC) & ArborFirmaCraft (AFC)
+        if (baseRes.getNamespace().matches("tfc|afc")) {
+            if (path.matches("rock/bricks/\\w+") && baseblock.defaultBlockState().instrument() == NoteBlockInstrument.BASEDRUM) {
+                int index = path.lastIndexOf("/");
+                String stoneName = path.substring(index + 1); // Get granite from tfc:rock/bricks/granite
+                var opt = BuiltInRegistries.BLOCK.getOptional(
+                        new ResourceLocation(baseRes.getNamespace(), path.replace("bricks", "raw"))
+                );
+//                Moonlight.LOGGER.warn("STONER: {} - {}", stoneName, baseRes);
+                if (opt.isPresent()) {
+                    return Optional.of(new StoneType(baseRes.withPath(stoneName), opt.get()));
+                }
+            }
+        }
+
+        if (path.endsWith("_bricks") && baseblock.defaultBlockState().instrument() == NoteBlockInstrument.BASEDRUM) {
             String stoneName = path.substring(0, path.length() - 7);
-            var opt = BuiltInRegistries.BLOCK.getOptional(blockId.withPath(stoneName));
+            var opt = BuiltInRegistries.BLOCK.getOptional(baseRes.withPath(stoneName));
             if (opt.isPresent()) {
-                return Optional.of(new StoneType(blockId.withPath(stoneName), opt.get()));
+                return Optional.of(new StoneType(baseRes.withPath(stoneName), opt.get()));
             }
         }
         return Optional.empty();
