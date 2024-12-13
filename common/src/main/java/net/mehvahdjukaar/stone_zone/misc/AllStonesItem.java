07de4111216cc1025317dc7d0b576b13e9acec53
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2024-12-13 00:03:14 -0700

    AllStonesItem: Added CLIENT annotation & should Fix #4

--- common/src/main/java/net/mehvahdjukaar/stone_zone/misc/AllStonesItem.java
+++ common/src/main/java/net/mehvahdjukaar/stone_zone/misc/AllStonesItem.java
@@ -1,8 +1,9 @@
 package net.mehvahdjukaar.stone_zone.misc;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.mehvahdjukaar.moonlight.api.client.ICustomItemRendererProvider;
 import net.mehvahdjukaar.moonlight.api.client.ItemStackRenderer;
-import net.mehvahdjukaar.stone_zone.type.StoneType;
 import net.minecraft.world.item.Item;
 
 import java.util.function.Supplier;
@@ -13,6 +14,8 @@ public class AllStonesItem extends Item implements ICustomItemRendererProvider {
         super(new Item.Properties());
     }
 
+    @Override
+    @Environment(EnvType.CLIENT)
     public Supplier<ItemStackRenderer> getRendererFactory() {
         return StoneTypeCycleItemRenderer::new;
     }
