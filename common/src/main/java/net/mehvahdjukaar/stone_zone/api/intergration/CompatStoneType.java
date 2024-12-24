5d44d504b66e7243a57a9c4ef82ea9e7a69586bc
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2024-12-24 11:54:24 -0700

    CompatStoneType: Added multiple stone mods & Close #17

--- common/src/main/java/net/mehvahdjukaar/stone_zone/api/intergration/CompatStoneType.java
+++ common/src/main/java/net/mehvahdjukaar/stone_zone/api/intergration/CompatStoneType.java
@@ -3,22 +3,39 @@ package net.mehvahdjukaar.stone_zone.api.intergration;
 import net.mehvahdjukaar.moonlight.api.set.BlockSetAPI;
 import net.mehvahdjukaar.stone_zone.api.set.StoneType;
 
+    /// StoneType Detection detect a StoneType that met 2 requirements:
+        /// BASS_DRUM (sound)
+        /// blockID: nameStoneType_bricks - only 2 words
 // Put all undetected StoneTypes (including hardcoded ones) from mods in here to be included
 public class CompatStoneType {
 
     public static void init() {
 
+        // Create - undetected due to no BRICKS
+        simpleFinder("create", "limestone");
+        simpleFinder("create", "asurine");
+        simpleFinder("create", "crimsite");
+        simpleFinder("create", "ochrum");
+        simpleFinder("create", "veridium");
+        simpleFinder("create", "scoria");
+        simpleFinder("create", "scorchia");
+
         // Aether Redux - undetected due to HARP sound
         simpleFinder("aether_redux", "sentrite");
         simpleFinder("aether_redux", "divinite");
+        simpleFinder("aether_redux", "driftshale"); // - due to no BRICKS
 
         // Aether Works - undetected due to HARP sound
         simpleFinder("aetherworks", "suevite");
 
-        // Deeper And Darker
+        // Deep Aether - undetected due to no BRICKS
+        simpleFinder("deep_aether", "clorite");
+        simpleFinder("deep_aether", "raw_clorite");
+
+        // Deeper And Darker - undetected due to 3 words (sculk_stone_bricks)
         simpleFinder("deeperdarker", "sculk_stone");
 
-        // Rebinated Nether
+        // Rubinated Nether - undetected due to 3 words (altar_stone_bricks)
         simpleFinder("rubinated_nether", "altar_stone");
 
         // Alex's Caves - undetected due to HARP sound
@@ -27,7 +44,7 @@ public class CompatStoneType {
         simpleFinder("alexscaves", "abyssmarine");
         simpleFinder("alexscaves", "guanostone");
 
-        // Enlightened End
+        // Enlightened End - undetected due to 3 words (void_shale_bricks)
         simpleFinder("enlightened_end", "void_shale");
 
 
