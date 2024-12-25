e3194c6aebd5a6055c2f987c1a6c23b2e277fb9f
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2024-12-24 21:08:08 -0700

    SZModule: Added HardcodedBlockType.isStoneBlockAlreadyRegistered()

---
+++ common/src/main/java/net/mehvahdjukaar/stone_zone/misc/HardcodedBlockType.java
@@ -0,0 +1,64 @@
+package net.mehvahdjukaar.stone_zone.misc;
+
+import net.mehvahdjukaar.moonlight.api.set.wood.WoodType;
+import net.mehvahdjukaar.stone_zone.api.set.StoneType;
+import org.jetbrains.annotations.Nullable;
+
+public class HardcodedBlockType {
+
+    public static String stoneidentify;
+    public static String StoneTypeFromMod;
+    public static String modId;
+    public static String supportedBlockName;
+    public static String shortenedIdenfity;
+
+    @Nullable
+    public static Boolean isStoneBlockAlreadyRegistered(String blockName, StoneType stoneType, String ModId, String shortenedId) {
+        stoneidentify = stoneType.getId().toString();
+        StoneTypeFromMod = stoneType.getNamespace();
+        modId = ModId;
+        supportedBlockName = blockName;
+        shortenedIdenfity = shortenedId;
+
+            /// ========== EXCLUDE ========== \\\
+        // EXAMPLE
+        if (isStoneRegistryOf("", "", "", "", "")) return true;
+
+            /// ========== INCLUDE ========== \\\
+        // EXAMPLE
+        if (isStoneRegistryOf("", "", "", "", "")) return false;
+
+
+        return null;
+    }
+
+    public static Boolean isStoneRegistryOf(String whichSupportedModId, String shortenedId, String stonetypeFromMod, String stoneTypeId, String whichSupportedBlockName) {
+
+        String[] expressions = {
+                whichSupportedModId,
+                shortenedId,
+                stonetypeFromMod,
+                stoneTypeId,
+                whichSupportedBlockName
+        };
+
+        String[] values = {
+                modId,
+                shortenedIdenfity,
+                StoneTypeFromMod,
+                stoneidentify,
+                supportedBlockName
+        };
+
+        for (int idx = 0; idx < values.length; idx++ ) {
+
+            if (!expressions[idx].isEmpty()) { // Skip the blank expressions
+                boolean isNotMatched = !values[idx].matches(expressions[idx]);
+                if (isNotMatched) return false;
+            }
+        }
+
+        return true;
+    }
+
+}
