15aaee1188aaf764f29d23969971b7b756648ed2
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-01-13 01:20:14 -0700

    NEW CLASSES: MudType & MudTypeRegistry

---
+++ common/src/main/java/net/mehvahdjukaar/stone_zone/api/set/MudType.java
@@ -0,0 +1,88 @@
+package net.mehvahdjukaar.stone_zone.api.set;
+
+import net.mehvahdjukaar.moonlight.api.platform.PlatHelper;
+import net.mehvahdjukaar.moonlight.api.set.BlockType;
+import net.mehvahdjukaar.stone_zone.StoneZone;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
+import org.jetbrains.annotations.ApiStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+public class MudType extends StoneType {
+
+    public final Block mud;
+
+    protected MudType(ResourceLocation id, Block mud) {
+        super(id, mud);
+        this.mud = mud;
+    }
+
+    @Override
+    public String getTranslationKey() {
+        return "mud_type." + this.getNamespace() + "." + this.getTypeName();
+    }
+
+    @Override
+    protected void initializeChildrenBlocks() {
+        super.initializeChildrenBlocks();
+        this.addChild("packed", this.findRelatedEntry("packed", BuiltInRegistries.BLOCK));
+    }
+
+    public static class Finder implements SetFinder<MudType> {
+
+        private final Map<String, ResourceLocation> childNames = new HashMap<>();
+        private final Supplier<Block> mudFinder;
+        private final ResourceLocation id;
+
+        public Finder(ResourceLocation id, Supplier<Block> mud) {
+            this.id = id;
+            this.mudFinder = mud;
+        }
+
+        public static Finder vanilla(String nameMud){
+            return simple("minecraft", nameMud, nameMud);
+        }
+
+        public static Finder simple(String modId, String nameMudType, String nameMud) {
+            return simple(new ResourceLocation(modId, nameMudType), new ResourceLocation(modId, nameMud));
+        }
+
+        public static Finder simple(ResourceLocation nameMudTYpe, ResourceLocation nameMud) {
+            return new Finder(nameMudTYpe,
+                    () -> BuiltInRegistries.BLOCK.get(nameMud));
+        }
+
+        public void addChild(String childType, String childName) {
+            addChild(childType, new ResourceLocation(id.getNamespace(), childName));
+        }
+
+        public void addChild(String childType, ResourceLocation childName) {
+            this.childNames.put(childType, childName);
+        }
+
+        @Override
+        @ApiStatus.Internal
+        public Optional<MudType> get() {
+            if (PlatHelper.isModLoaded(id.getNamespace())) {
+                try {
+                    Block mud = mudFinder.get();
+                    var d = BuiltInRegistries.BLOCK.get(BuiltInRegistries.BLOCK.getDefaultKey());
+                    if (mud != d && mud != null) {
+                        var w = new MudType(id, mud);
+                        childNames.forEach((key, value) -> w.addChild(key, BuiltInRegistries.BLOCK.get(value)));
+                        return Optional.of(w);
+                    }
+                } catch (Exception ignored) {
+                }
+                StoneZone.LOGGER.warn("Failed to find custom mud type {}", id);
+            }
+            return Optional.empty();
+        }
+    }
+
+}
---
+++ common/src/main/java/net/mehvahdjukaar/stone_zone/api/set/MudTypeRegistry.java
@@ -0,0 +1,64 @@
+package net.mehvahdjukaar.stone_zone.api.set;
+
+import net.mehvahdjukaar.moonlight.api.events.AfterLanguageLoadEvent;
+import net.mehvahdjukaar.moonlight.api.set.BlockTypeRegistry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public class MudTypeRegistry extends BlockTypeRegistry<MudType> {
+
+    public static final MudTypeRegistry INSTANCE = new MudTypeRegistry();
+
+    public MudTypeRegistry() {
+        super(MudType.class, "mud_type");
+
+        this.addFinder(MudType.Finder.vanilla("mud"));
+    }
+
+    public static MudType getMudType() {
+        return getValue("mud");
+    }
+
+    public static MudType getValue(String mudTypeId) {
+        return INSTANCE.get(new ResourceLocation(mudTypeId));
+    }
+
+    @Override
+    public MudType getDefaultType() {
+        return getMudType();
+    }
+
+    @Override
+    public Optional<MudType> detectTypeFromBlock(Block baseblock, ResourceLocation baseRes) {
+        String path = baseRes.getPath();
+
+        if (path.matches("[a-z]+_mud_bricks") && baseblock.defaultBlockState().instrument() == NoteBlockInstrument.BASEDRUM) {
+            String mudName = path.substring(0, path.length() - 7); // get mudName from namespace:mudName_bricks
+            String mudAlt = mudName + "_mud"; // Some mods included "_mud" as the suffix
+            var opt = BuiltInRegistries.BLOCK.getOptional(baseRes.withPath(mudName));
+            var alt = BuiltInRegistries.BLOCK.getOptional(baseRes.withPath(mudAlt));
+            if (opt.isPresent()) return Optional.of(new MudType(baseRes.withPath(mudName), opt.get()));
+            else if (alt.isPresent()) return Optional.of(new MudType(baseRes.withPath(mudAlt), alt.get()));
+
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public void addTypeTranslations(AfterLanguageLoadEvent language) {
+        getValues().forEach((mudType) -> {
+            if (language.isDefault()) language.addEntry(mudType.getTranslationKey(), mudType.getReadableName());
+        });
+    }
+
+    @Override
+    public int priority() {
+        return 110;
+    }
+}
