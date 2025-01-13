af06e072573e88b482a1cf2aec8f6262bb46b69b
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-01-13 01:20:52 -0700

    NEW MODULE: QuarkMudModule

---
+++ common/src/main/java/net/mehvahdjukaar/stone_zone/modules/quark/QuarkMudModule.java
@@ -0,0 +1,89 @@
+package net.mehvahdjukaar.stone_zone.modules.quark;
+
+import net.mehvahdjukaar.every_compat.api.RenderLayer;
+import net.mehvahdjukaar.every_compat.api.SimpleEntrySet;
+import net.mehvahdjukaar.moonlight.api.util.Utils;
+import net.mehvahdjukaar.stone_zone.api.SZModule;
+import net.mehvahdjukaar.stone_zone.api.set.MudType;
+import net.mehvahdjukaar.stone_zone.api.set.MudTypeRegistry;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.level.block.Block;
+import org.violetmoon.quark.content.building.block.MudBrickLatticeBlock;
+import org.violetmoon.quark.content.building.module.MoreMudBlocksModule;
+import org.violetmoon.zeta.block.ZetaBlock;
+import org.violetmoon.zeta.block.ZetaPillarBlock;
+
+import static net.mehvahdjukaar.every_compat.common_classes.Utilities.copyChildrenPropertySafe;
+
+
+//SUPPORT: v4.0-4.6.0+
+public class QuarkMudModule extends SZModule {
+
+    public final SimpleEntrySet<MudType, Block> brick_lattices;
+    public final SimpleEntrySet<MudType, Block> carved_bricks;
+    public final SimpleEntrySet<MudType, Block> pillars;
+
+    public QuarkMudModule(String modId) {
+        super(modId, "q");
+        ResourceKey<CreativeModeTab> tab = CreativeModeTabs.BUILDING_BLOCKS;
+
+        brick_lattices = QuarkEntrySet.of(MudType.class, "brick_lattice", MoreMudBlocksModule.class,
+                        getModBlock("mud_brick_lattice"), MudTypeRegistry::getMudType,
+                        mudType -> new MudBrickLatticeBlock(null, copyChildrenPropertySafe("bricks", mudType))
+                )
+                .createPaletteFromBricks()
+                .requiresChildren("bricks") //REASON: recipes, palettes
+                .addTexture(modRes("block/mud_brick_lattice"))
+                .addTag(BlockTags.MINEABLE_WITH_PICKAXE, Registries.BLOCK)
+                .setTabKey(tab)
+                .addRecipe(modRes("building/stonecutting/mud_brick_lattice_stonecutter"))
+                .addRecipe(modRes("building/crafting/mud_brick_lattice"))
+                .setRenderType(RenderLayer.TRANSLUCENT)
+                .build();
+        this.addEntry(brick_lattices);
+
+        carved_bricks = QuarkEntrySet.of(MudType.class, "bricks", "carved", MoreMudBlocksModule.class,
+                        getModBlock("carved_mud_bricks"), MudTypeRegistry::getMudType,
+                        mudType -> {
+                            String name = shortenedId() + "/" + mudType.getAppendableIdWith("carved", "bricks");
+                            return new ZetaBlock(name, null, copyChildrenPropertySafe("bricks", mudType));
+                        }
+                )
+                .createPaletteFromBricks()
+                .requiresChildren("brick_slab", "bricks") //REASON: recipes, textures, palettes
+                //TEXTURES: bricks
+                .addTexture(modRes("block/carved_mud_bricks"))
+                .addTag(BlockTags.MINEABLE_WITH_PICKAXE, Registries.BLOCK)
+                .addTag(new ResourceLocation("architects_palette:wizard_blocks"), Registries.BLOCK)
+                .setTabKey(tab)
+                .addRecipe(modRes("building/stonecutting/carved_mud_bricks_stonecutter"))
+                .addRecipe(modRes("building/crafting/carved_mud_bricks"))
+                .build();
+        this.addEntry(carved_bricks);
+
+        pillars = QuarkEntrySet.of(MudType.class, "pillar", MoreMudBlocksModule.class,
+                        getModBlock("mud_pillar"), MudTypeRegistry::getMudType,
+                        mudType -> {
+                            String name = shortenedId() + "/" + mudType.getAppendableIdWith("pillar");
+                            return new ZetaPillarBlock(name, null, Utils.copyPropertySafe(mudType.mud));
+                        }
+                )
+                .createPaletteFromBricks()
+                .requiresChildren("brick_slab", "bricks") //REASON: recipes, palettes
+                .addTexture(modRes("block/mud_pillar"))
+                .addTexture(modRes("block/mud_pillar_top"))
+                .addTag(BlockTags.MINEABLE_WITH_PICKAXE, Registries.BLOCK)
+                .addTag(new ResourceLocation("architects_palette:wizard_blocks"), Registries.BLOCK)
+                .setTabKey(tab)
+                .addRecipe(modRes("building/crafting/mud_pillar"))
+                .addRecipe(modRes("building/stonecutting/mud_pillar_stonecutter"))
+                .build();
+        this.addEntry(pillars);
+
+    }
+}
