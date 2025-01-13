3b1971b9ea79bb44bb2a9c197622419a4058ffbc
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-01-13 01:23:04 -0700

    CompatStoneType: Added Deeper-And-Darker & Renamed methods

--- common/src/main/java/net/mehvahdjukaar/stone_zone/api/intergration/CompatStoneType.java
+++ common/src/main/java/net/mehvahdjukaar/stone_zone/api/intergration/CompatStoneType.java
@@ -1,6 +1,7 @@
 package net.mehvahdjukaar.stone_zone.api.intergration;
 
 import net.mehvahdjukaar.moonlight.api.set.BlockSetAPI;
+import net.mehvahdjukaar.stone_zone.api.set.MudType;
 import net.mehvahdjukaar.stone_zone.api.set.StoneType;
 
     /// StoneType Detection detect a StoneType that met 2 requirements:
@@ -11,67 +12,72 @@ public class CompatStoneType {
 
     public static void init() {
 
+        // Deeper And Darker
+        simpleMudFinder("deeperdarker", "sculk_grime");
+
+        // What Is Stone
+        simpleStoneFinder("what_is_stone", "anthracite");
+        simpleStoneFinder("what_is_stone", "breccia");
+        simpleStoneFinder("what_is_stone", "conglomerate");
+        simpleStoneFinder("what_is_stone", "pegmatite");
+        simpleStoneFinder("what_is_stone", "scoria");
+
         // BetterEnd
-        simpleFinder("betterend", "azure_jadestone");
-        simpleFinder("betterend", "sandy_jadestone");
-        simpleFinder("betterend", "virid_jadestone");
-        simpleFinder("betterend", "sulphuric_rock");
+        simpleStoneFinder("betterend", "azure_jadestone");
+        simpleStoneFinder("betterend", "sandy_jadestone");
+        simpleStoneFinder("betterend", "virid_jadestone");
+        simpleStoneFinder("betterend", "sulphuric_rock");
 
         // Create - undetected due to no BRICKS
-        simpleFinder("create", "limestone");
-        simpleFinder("create", "asurine");
-        simpleFinder("create", "crimsite");
-        simpleFinder("create", "ochrum");
-        simpleFinder("create", "veridium");
-        simpleFinder("create", "scoria");
-        simpleFinder("create", "scorchia");
+        simpleStoneFinder("create", "limestone");
+        simpleStoneFinder("create", "asurine");
+        simpleStoneFinder("create", "crimsite");
+        simpleStoneFinder("create", "ochrum");
+        simpleStoneFinder("create", "veridium");
+        simpleStoneFinder("create", "scoria");
+        simpleStoneFinder("create", "scorchia");
 
         // Create Dreams & Desires
-        simpleFinder("create_dd", "gabbro");
-        simpleFinder("create_dd", "weathered_limestone");
-        simpleFinder("create_dd", "aethersite");
-        simpleFinder("create_dd", "potassic");
+        simpleStoneFinder("create_dd", "gabbro");
+        simpleStoneFinder("create_dd", "weathered_limestone");
+        simpleStoneFinder("create_dd", "aethersite");
+        simpleStoneFinder("create_dd", "potassic");
 
         // Bountiful Fares - nameStone of feldspar is feldspar_block
         stoneBlockFinder("bountifulfares", "feldspar");
 
         // Aether Redux - undetected due to HARP sound
-        simpleFinder("aether_redux", "sentrite");
-        simpleFinder("aether_redux", "divinite");
-        simpleFinder("aether_redux", "driftshale"); // - due to no BRICKS
+        simpleStoneFinder("aether_redux", "sentrite");
+        simpleStoneFinder("aether_redux", "divinite");
+        simpleStoneFinder("aether_redux", "driftshale"); // - due to no BRICKS
 
         // Aether Works - undetected due to HARP sound
-        simpleFinder("aetherworks", "suevite");
+        simpleStoneFinder("aetherworks", "suevite");
 
         // Deep Aether - undetected due to no BRICKS
-        simpleFinder("deep_aether", "clorite");
-        simpleFinder("deep_aether", "raw_clorite");
-
-        // Deeper And Darker - undetected due to 3 words (sculk_stone_bricks)
-        simpleFinder("deeperdarker", "sculk_stone");
-
-        // Rubinated Nether - undetected due to 3 words (altar_stone_bricks)
-        simpleFinder("rubinated_nether", "altar_stone");
+        simpleStoneFinder("deep_aether", "clorite");
+        simpleStoneFinder("deep_aether", "raw_clorite");
 
         // Alex's Caves - undetected due to HARP sound
-        simpleFinder("alexscaves", "galena");
-        simpleFinder("alexscaves", "radrock");
-        simpleFinder("alexscaves", "abyssmarine");
-        simpleFinder("alexscaves", "guanostone");
-        simpleFinder("alexscaves", "limestone"); // - due to no BRICKS
+        simpleStoneFinder("alexscaves", "galena");
+        simpleStoneFinder("alexscaves", "radrock");
+        simpleStoneFinder("alexscaves", "abyssmarine");
+        simpleStoneFinder("alexscaves", "guanostone");
+        simpleStoneFinder("alexscaves", "limestone"); // - due to no BRICKS
 
         // Enlightened End - undetected due to 3 words (void_shale_bricks)
-        simpleFinder("enlightened_end", "void_shale");
+        simpleStoneFinder("enlightened_end", "void_shale");
 
         // Ars Nouveau
-        simpleFinder("ars_nouveau", "sourcestone");
+        simpleStoneFinder("ars_nouveau", "sourcestone");
 
         // Quark
-        simpleFinder("quark", "soul_sandstone");
+        simpleStoneFinder("quark", "soul_sandstone");
 
     }
 
-    public static void simpleFinder(String modId, String nameStoneType) {
+//!! StoneType
+    public static void simpleStoneFinder(String modId, String nameStoneType) {
         var stonetypeFinder = StoneType.Finder.simple(modId, nameStoneType, nameStoneType);
 
         BlockSetAPI.addBlockTypeFinder(StoneType.class, stonetypeFinder);
@@ -83,4 +89,12 @@ public class CompatStoneType {
 
         BlockSetAPI.addBlockTypeFinder(StoneType.class, stonetypeFinder);
     }
+
+//!! MudType
+    public static void simpleMudFinder(String modId, String nameStoneType) {
+        var stonetypeFinder = MudType.Finder.simple(modId, nameStoneType, nameStoneType);
+
+        BlockSetAPI.addBlockTypeFinder(MudType.class, stonetypeFinder);
+    }
+
 }
