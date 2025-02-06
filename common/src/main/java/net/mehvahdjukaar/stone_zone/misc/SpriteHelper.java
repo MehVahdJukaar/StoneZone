41c80bf0fc621b1ee5be53b11878a5800fa8995e
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-02-05 18:46:19 -0700

    SpriteHelper: Added model methods

--- common/src/main/java/net/mehvahdjukaar/stone_zone/misc/SpriteHelper.java
+++ common/src/main/java/net/mehvahdjukaar/stone_zone/misc/SpriteHelper.java
@@ -2,18 +2,32 @@ package net.mehvahdjukaar.stone_zone.misc;
 
 
 import net.mehvahdjukaar.moonlight.api.client.TextureCache;
+import net.mehvahdjukaar.stone_zone.StoneZone;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SpriteHelper {
+
+    public final static Map<ResourceLocation, String> modelID = new HashMap<>();
 
     public static void addHardcodedSprites() {
 
-        // The-Outer-End
-        addOptional("outerend:himmel_block", "all", "outerend:block/himmel_block");
-
+                //!! MODELS !!\\
+/// NOTE: Used to identify StoneTypes' model only based off their name
         // BetterEnd
-        addOptional("betterend:umbralith", "all", "betterend:block/umbralith_1");
+        addToModelId("bbb/betterend/umbralith_layer", "betterend:block/umbralith_1");
+
+        // Eternal Tales
+        addToModelId("bbb/eternal_tales/smooth_purgatorium_stone_layer", "eternal_tales:block/purgatorium_stone_smooth");
+        addToModelId("bbb/eternal_tales/smooth_rajiit_layer", "eternal_tales:block/rajiit_smooth");
+
+                //!! TEXTURES !!\\
+/// NOTE: Used to identify StoneTypes' texture only based off their name
+        // BetterEnd
+        addOptional("betterend:umbralith", "all", "betterend:block/umbralith");
 
         // Eternal Tales
         addOptional("eternal_tales:purgatorium_stone", "all", "eternal_tales:block/purgstone");
@@ -23,5 +37,11 @@ public class SpriteHelper {
     private static void addOptional(String blockId, String textureId, String texturePath) {
         BuiltInRegistries.BLOCK.getOptional(new ResourceLocation(blockId))
                 .ifPresent(b -> TextureCache.registerSpecialTextureForBlock(b, textureId, new ResourceLocation(texturePath)));
+    }
+
+    private static void addToModelId(String blockid, String pathModel) {
+        modelID.put(StoneZone.res(blockid), pathModel);
+
+
     }
 }
