332082106588fcdeb4c22c51969f7b2446ea3f8e
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-02-16 01:30:21 -0700

    HardcodedBlockType: Added Quark|Wetland-Whimsy with Create & Fixed #64

--- common/src/main/java/net/mehvahdjukaar/stone_zone/misc/HardcodedBlockType.java
+++ common/src/main/java/net/mehvahdjukaar/stone_zone/misc/HardcodedBlockType.java
@@ -46,6 +46,9 @@ public class HardcodedBlockType {
         // The stone_squares block from Blockus is why stone_squares from Rechiseled got skipped
         if (isStoneRegistryOf("rechiseled", "", "blockus", "", "\\w+_squares")) return false;
 
+        // Create's blocks aren't generated for Quark & Wetland-Whimsy because they both have LIMESTONE & Also fix the tag issue (#64)
+        if (isStoneRegistryOf("create", "", "", "quark:limestone|wetland_whimsy:limestone", "")) return false;
+
 
         return null;
     }
