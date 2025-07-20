503af3d0bb381602d8f0db8d58e029bbc8248e2a
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-07-20 17:39:50 -0600

    HardcodedBlockType: Removed shortenedId

--- common/src/main/java/net/mehvahdjukaar/stone_zone/misc/HardcodedBlockType.java
+++ common/src/main/java/net/mehvahdjukaar/stone_zone/misc/HardcodedBlockType.java
@@ -11,7 +11,6 @@ public class HardcodedBlockType {
     public static String StoneTypeFromMod;
     public static String modId;
     public static String supportedBlockName;
-    public static String shortenedIdenfity;
 
     public static final Set<String> BLACKLISTED_MODS = Set.of(
             "immersive_weathering", "chipped", "create_confectionery", "rgbblocks"
@@ -28,55 +27,50 @@ public class HardcodedBlockType {
     );
 
     @Nullable
-    public static Boolean isStoneBlockAlreadyRegistered(String blockName, StoneType stoneType, String ModId, String shortenedId) {
+    public static Boolean isStoneBlockAlreadyRegistered(String blockName, StoneType stoneType, String ModId) {
         stoneidentify = stoneType.getId().toString();
         StoneTypeFromMod = stoneType.getNamespace();
         modId = ModId;
         supportedBlockName = blockName;
-        shortenedIdenfity = shortenedId;
 
             /// ========== INCLUDE VANILLA TYPE ========== \\\
         // Include minecraft's PRISMARINE with Waystones
-        if (isStoneFrom("waystones", "", "", "", "prismarine_waystone")) return false;
+        if (isStoneFrom("waystones", "", "", "prismarine_waystone")) return false;
 
             /// ========== EXCLUDE ========== \\\
-        // EXAMPLE
-//        if (isStoneRegistryOf("create", "create", "create:limestone", "limestone_pillar")) return true;
-
         // Exclude all of Vanilla Types
         if (stoneType.isVanilla()) return true;
 
         // The StoneType's texture is only white and no way for blocks to copy its color behavior
-        if (isStoneFrom("", "", "", "rgbblocks:prismarine", "")) return true;
+        if (isStoneFrom("", "", "rgbblocks:prismarine", "")) return true;
 
         // Create: Dreams & Desires' cut_stone_bricks shouldn't be detected but was
-        if (isStoneFrom("", "", "", "create_dd:cut_stone", "")) return true;
+        if (isStoneFrom("", "", "create_dd:cut_stone", "")) return true;
 
         // Stone Expansion's stone is based on Minecraft's stone and shouldn't be included
-        if (isStoneFrom("", "", "", "stoneexpansion:(cut|mossy|smooth|polished)_stone", "")) return true;
+        if (isStoneFrom("", "", "stoneexpansion:(cut|mossy|smooth|polished)_stone", "")) return true;
 
             /// ========== INCLUDE ========== \\\
         // EXAMPLE
 //        if (isStoneRegistryOf("create", "c", "create", "create:limestone", "limestone_pillar")) return false;
 
         // Create's cut wasn't generated due to Quark's cut_soul_sandstone
-        if (isStoneFrom("create", "", "quark", "", "cut_soul_sandstone")) return false;
+        if (isStoneFrom("create", "quark", "", "cut_soul_sandstone")) return false;
 
         // The stone_squares block from Blockus is why stone_squares from Rechiseled got skipped
-        if (isStoneFrom("rechiseled", "", "blockus", "", "squares")) return false;
+        if (isStoneFrom("rechiseled", "blockus", "", "squares")) return false;
 
         // Create's blocks aren't generated for Quark, Wetland-Whimsy, Geologic-Expansion because they both have LIMESTONE & Also fix the tag issue (#64)
-        if (isStoneFrom("create", "", "", "quark:limestone|wetland_whimsy:limestone|geologicexpansion:limestone", "")) return false;
+        if (isStoneFrom("create", "", "quark:limestone|wetland_whimsy:limestone|geologicexpansion:limestone", "")) return false;
 
 
         return null;
     }
 
-    public static Boolean isStoneFrom(String whichSupportedModId, String shortenedId, String stonetypeFromMod, String stoneTypeId, String whichSupportedBlockName) {
+    public static Boolean isStoneFrom(String supportedModId, String stonetypeFromMod, String stoneTypeId, String whichSupportedBlockName) {
 
         String[] expressions = {
-                whichSupportedModId,
-                shortenedId,
+                supportedModId,
                 stonetypeFromMod,
                 stoneTypeId,
                 whichSupportedBlockName
@@ -84,7 +78,6 @@ public class HardcodedBlockType {
 
         String[] values = {
                 modId,
-                shortenedIdenfity,
                 StoneTypeFromMod,
                 stoneidentify,
                 supportedBlockName
