cd322c0f5b36f1abbbc8b3b6d92a5e0fc8963acb
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2025-09-02 04:13:50 -0600

    MULTIPLE CLASSES: Ported from 1.20.1 with updates

--- common/src/main/java/net/mehvahdjukaar/stone_zone/StoneZone.java
+++ common/src/main/java/net/mehvahdjukaar/stone_zone/StoneZone.java
@@ -1,21 +1,22 @@
 package net.mehvahdjukaar.stone_zone;
 
 import net.mehvahdjukaar.every_compat.EveryCompat;
+import net.mehvahdjukaar.every_compat.api.CompatModule;
 import net.mehvahdjukaar.every_compat.dynamicpack.ServerDynamicResourcesHandler;
-import net.mehvahdjukaar.moonlight.api.platform.ClientHelper;
 import net.mehvahdjukaar.moonlight.api.platform.PlatHelper;
 import net.mehvahdjukaar.moonlight.api.set.BlockSetAPI;
 import net.mehvahdjukaar.stone_zone.api.intergration.CompatStoneType;
-import net.mehvahdjukaar.stone_zone.api.set.MudTypeRegistry;
-import net.mehvahdjukaar.stone_zone.api.set.StoneTypeRegistry;
+import net.mehvahdjukaar.stone_zone.api.set.mud.MudTypeRegistry;
+import net.mehvahdjukaar.stone_zone.api.set.stone.StoneTypeRegistry;
 import net.mehvahdjukaar.stone_zone.configs.SZConfigs;
-import net.mehvahdjukaar.stone_zone.misc.ModelUtils;
-import net.mehvahdjukaar.stone_zone.misc.SpriteHelper;
+import net.mehvahdjukaar.stone_zone.configs.UnsafeDisablerConfigs;
+import net.mehvahdjukaar.stone_zone.misc.CompatSpritesHelper;
 import net.minecraft.resources.ResourceLocation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class StoneZone extends EveryCompat {
     public static final String MOD_ID = "stonezone";
@@ -23,21 +24,15 @@ public class StoneZone extends EveryCompat {
 
     public static void init() {
         SZConfigs.init();
+        UnsafeDisablerConfigs.init();
         SZRegistry.init();
+        CompatSpritesHelper.initHardcodedSprite();
 
         BlockSetAPI.registerBlockSetDefinition(StoneTypeRegistry.INSTANCE);
         BlockSetAPI.registerBlockSetDefinition(MudTypeRegistry.INSTANCE);
         CompatStoneType.init();
 
-        PlatHelper.addCommonSetup(SpriteHelper::addHardcodedSprites);
-
-        if (PlatHelper.getPhysicalSide().isClient()) {
-            ClientHelper.addClientReloadListener(() -> (preparationBarrier, resourceManager, preparationsProfiler, reloadProfiler, backgroundExecutor, gameExecutor) ->
-                            CompletableFuture.completedFuture(null)
-                                    .thenCompose(preparationBarrier::wait)
-                                    .thenAcceptAsync((object) -> ModelUtils.reset(), gameExecutor),
-                    res("stonezone_reloader"));
-        }
+        PlatHelper.addCommonSetup(CompatSpritesHelper::addHardcodedModel);
 
         // Add ModId to DynamicPack so the tags can be loaded into the world first time
         addModToDynamicPack("caverns_and_chasms");
@@ -55,5 +50,17 @@ public class StoneZone extends EveryCompat {
         if (PlatHelper.isModLoaded(modId)) {
             ServerDynamicResourcesHandler.INSTANCE.dynamicPack.addNamespaces(modId);
         }
+    }
+
+    @SafeVarargs
+    public static void addMultipleIfLoaded(String modId, Supplier<Function<String, CompatModule>>... moduleFactories) {
+        if (PlatHelper.isModLoaded(modId)) {
+            CompatModule module;
+            for (var moduleFactory : moduleFactories) {
+                module = moduleFactory.get().apply(modId);
+                addModule(module);
+            }
+        }
+
     }
 }
---
+++ common/src/main/java/net/mehvahdjukaar/stone_zone/configs/UnsafeDisablerConfigs.java
@@ -0,0 +1,94 @@
+package net.mehvahdjukaar.stone_zone.configs;
+
+import net.mehvahdjukaar.every_compat.EveryCompat;
+import net.mehvahdjukaar.moonlight.api.platform.configs.ConfigBuilder;
+import net.mehvahdjukaar.moonlight.api.platform.configs.ConfigType;
+import net.mehvahdjukaar.moonlight.api.platform.configs.ModConfigHolder;
+import net.mehvahdjukaar.stone_zone.StoneZone;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+public class UnsafeDisablerConfigs {
+
+    public static Supplier<List<String>> stoneTypeList;
+    public static Supplier<List<String>> mudTypeList;
+    public static Supplier<List<String>> entrySetList;
+
+    public static ModConfigHolder CONFIG_SPEC;
+
+    public static void init() {}
+
+    static {
+
+        ConfigBuilder builder = ConfigBuilder.create(StoneZone.res("hazardous"), ConfigType.COMMON);
+
+        String comment = """
+                    ═════════════════════════ Attention ═════════════════════════
+                    Don't use this if you don't know what you are doing
+                        REASON:
+                    This file is a conditional registration. This is harmless in Singleplayer World,
+                    but harmful in SERVER because you won't able to join.
+
+                    ══════════════════════════ Detail ═══════════════════════════
+                    This file allow you to exclude StoneTypes, MudType, EntrySet
+                    1) You can find their names for StoneTypes, MudType, or EntrySet in `everycomp-entries.toml`
+                    2) Leave a value empty to disable that rule.
+
+                    Module - is a Supported Mod, just a modId is sufficient.
+                    EntrySet - is a FurnitureType or DecorativeType that Stone-Zone is supporting via the mod. it is either block or item.
+
+                    NOTE: blacklisting a Module will be applied to Wood-Good, Stone-Zone, Gems-Realm - if you want to blacklist, then use everycomp-hazardous.toml
+                """;
+        builder.comment(comment);
+
+        builder.push("stonetype");
+        String stoneTypeExample = """
+                    EXAMPLE: blacklist = [
+                        "forestry:.*fireproof.*",\t\tCOMMENT: .* is an RegEx, it exclude all of StoneType containing "fireproof" from Forestry
+                        "biomesoplenty:.*",\t\t\t\tCOMMENT: .* is an RegEx, it exclude all of StoneType from Wood Mod for any Module
+                        "biomesoplenty:redwood"\t\tCOMMENT: exclude redwood from Stone Mod for any module
+                    ]
+                """;
+        stoneTypeList = builder.comment("Exclude StoneType from all of Modules\n"+stoneTypeExample).define("blacklist", List.of());
+        builder.pop();
+
+        builder.push("mudtype");
+        mudTypeList = builder.comment("Exclude MudType from all of Modules\n\tThe example is same as StoneType's").define("blacklist", List.of());
+        builder.pop();
+
+        builder.push("entryset");
+        String entrysetExample = """
+                    This is only applied to Stone-Zone.
+                    EXAMPLE: blacklist = [
+                        "chipped:checkered_trapdoor",\tCOMMENT: chipped:checkered_oak_trapdoor without "oak"
+                        "variantvanillablocks:chest",\tCOMMENT: variantvanillablocks:oak_chest without "oak"
+                        "chipped:.*"\t\t\t\t\tCOMMENT: .* is an regex which will exclude all of EntrySets from one Module
+                    ]
+                """;
+        entrySetList = builder.comment("Exclude EntrySet from the module for All of StoneType or MudType\n"+entrysetExample).define("blacklist", List.of());
+        builder.pop();
+
+        CONFIG_SPEC = builder.build();
+
+        CONFIG_SPEC.forceLoad();
+
+        // Warning Message
+        if (!stoneTypeList.get().isEmpty() || !mudTypeList.get().isEmpty() || !entrySetList.get().isEmpty()) {
+            EveryCompat.LOGGER.warn("""
+                            \n
+                            ============================================================
+                            |                                                          |
+                            |                        ATTENTION                         |
+                            |  You are using conditional registration via Gems-Realm.  |
+                            |  Proceed at your own risk and do not complain if you     |
+                            |  CANNOT connect to servers                               |
+                            |                                                          |
+                            ============================================================
+                    """
+            );
+        }
+
+    }
+
+}
