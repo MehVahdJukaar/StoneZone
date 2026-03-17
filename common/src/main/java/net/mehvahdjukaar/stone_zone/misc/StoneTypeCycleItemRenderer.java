0a7043cb139070cce4d2783e936d22e82f025efa
Xel'Bayria <12866666+Xelbayria@users.noreply.github.com>
2026-03-16 23:45:56 -0600

    SZConfigs: Added 2 new configs for CYCLE_ITEM_RENDERER & Related #1203

--- common/src/main/java/net/mehvahdjukaar/stone_zone/misc/StoneTypeCycleItemRenderer.java
+++ common/src/main/java/net/mehvahdjukaar/stone_zone/misc/StoneTypeCycleItemRenderer.java
@@ -2,6 +2,13 @@ package net.mehvahdjukaar.stone_zone.misc;
 
 import net.mehvahdjukaar.every_compat.misc.BlockTypeCycleItemRenderer;
 import net.mehvahdjukaar.stone_zone.api.set.stone.StoneType;
+import net.mehvahdjukaar.stone_zone.configs.SZConfigs;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+
+import java.util.Objects;
 
 public class StoneTypeCycleItemRenderer extends BlockTypeCycleItemRenderer<StoneType> {
 
@@ -9,4 +16,18 @@ public class StoneTypeCycleItemRenderer extends BlockTypeCycleItemRenderer<Stone
         super(StoneType.class);
     }
 
+    @Override
+    @SuppressWarnings("ConstantValue")
+    public ItemStack getItemIcon() {
+        var itemId = SZConfigs.CREATIVE_TAB_ICON.get();
+        ItemStack item = BuiltInRegistries.ITEM.get(ResourceLocation.parse(itemId)).getDefaultInstance();
+
+        if (Objects.nonNull(item)) return item;
+        else return Items.BARRIER.getDefaultInstance();
+    }
+
+    @Override
+    public boolean getDisableCycleItemRenderer() {
+        return SZConfigs.DISABLE_CYCLE_ITEM_RENDERER.get();
+    }
 }
